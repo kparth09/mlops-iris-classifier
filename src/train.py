791adb1b3ace8992f838292ee045dd7125e2ad60
Parth Khandelwal <parthkhandelwal019@gmail.com>
2026-08-31 16:45:44 +0530

    Change n_estimators on branch A

--- src/train.py
+++ src/train.py
@@ -1,4 +1,3 @@
-import os
 import joblib
 
 from sklearn.datasets import load_iris
@@ -20,7 +19,7 @@ def load_data():
     return X_train, X_test, y_train, y_test
 
 
-def train_model(X_train, y_train, n_estimators=100, max_depth=None):
+def train_model(X_train, y_train, n_estimators=50, max_depth=None):
     model = RandomForestClassifier(
         n_estimators=n_estimators,
         max_depth=max_depth,
